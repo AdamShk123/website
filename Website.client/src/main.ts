2ebe39ac10f74f64a75212b700c17610f1c39c24
AdamShk123 <adamshkolnik5@gmail.com>
2024-12-07 22:49:46 -0600

    check fetch request

--- Website.client/src/main.ts
+++ Website.client/src/main.ts
@@ -5,5 +5,32 @@ const clickCount: HTMLDivElement | null = document.querySelector<HTMLDivElement>
 
 if(clickButton != null && clickCount != null) 
 {
-    clickButton.addEventListener('click', () => raiseCounter(clickCount));   
+    clickButton.addEventListener("click", () => raiseCounter(clickCount));   
 }
+
+const getData = async () => {
+    const url = "https://adamshkolnik.com/api/weatherforecast";
+    const init: RequestInit = { method: "GET" };
+    
+    try 
+    {
+        const response = await fetch(url, init);
+        
+        if(!response.ok)
+        {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const json = await response.json();
+        console.log(json);
+    }
+    catch (err: unknown) 
+    {
+        if (err instanceof Error) 
+        {
+            console.log(err.message);
+        }
+    }
+};
+
+getData().then(() => console.log("complete"));
