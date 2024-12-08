c69a3bb371109e4b10e34d7efde222628f9152a6
AdamShk123 <adamshkolnik5@gmail.com>
2024-12-08 15:45:23 -0600

    changed ci/cd files to only run for specific paths. added scss support

--- Website.client/src/main.ts
+++ Website.client/src/main.ts
@@ -8,29 +8,29 @@ if(clickButton != null && clickCount != null)
     clickButton.addEventListener("click", () => raiseCounter(clickCount));   
 }
 
-const getData = async () => {
-    const url = "https://adamshkolnik.com/api/weatherforecast";
-    const init: RequestInit = { method: "GET" };
-    
-    try 
-    {
-        const response = await fetch(url, init);
-        
-        if(!response.ok)
-        {
-            throw new Error(`Response status: ${response.status}`);
-        }
-        
-        const json = await response.json();
-        console.log(json);
-    }
-    catch (err: unknown) 
-    {
-        if (err instanceof Error) 
-        {
-            console.log(err.message);
-        }
-    }
-};
-
-getData().then(() => console.log("complete"));
+// const getData = async () => {
+//     const url = "https://adamshkolnik.com/api/weatherforecast";
+//     const init: RequestInit = { method: "GET" };
+//    
+//     try 
+//     {
+//         const response = await fetch(url, init);
+//        
+//         if(!response.ok)
+//         {
+//             throw new Error(`Response status: ${response.status}`);
+//         }
+//        
+//         const json = await response.json();
+//         console.log(json);
+//     }
+//     catch (err: unknown) 
+//     {
+//         if (err instanceof Error) 
+//         {
+//             console.log(err.message);
+//         }
+//     }
+// };
+//
+// getData().then(() => console.log("complete"));
--- Website.client/vite.config.ts
+++ Website.client/vite.config.ts
@@ -2,8 +2,14 @@ import {defineConfig} from "vite";
 
 export default defineConfig({
     test: {
-
         globals: true,
         environment: "jsdom",
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true
+            }
+        }
     }
 });
